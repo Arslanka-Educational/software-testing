76ac481c3dfd3909a395dbf079feb258414a39d9
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2024-04-13 20:26:43 +0300

    removed .idea

--- Lab_2/build.gradle.kts
+++ Lab_2/build.gradle.kts
@@ -16,13 +16,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.assertj:assertj-core:3.11.0")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("io.mockk:mockk:1.12.1")
-    implementation("org.assertj:assertj-core:3.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.0")
-    testImplementation("org.testng:testng:7.7.0")
 }
 
 tasks.test {
