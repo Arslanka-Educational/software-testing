d79e0ec484f837c196456e2159695399d1b1e10e
Zhenya Mint <zhenyamint@Zhenyas-Laptop.local>
2024-04-29 15:00:40 +0300

    fix main

--- Lab_2/src/main/kotlin/Main.kt
+++ Lab_2/src/main/kotlin/Main.kt
@@ -3,7 +3,9 @@ import math.functions.SeriesMathFunction
 import math.functions.complex.PiecewiseFunctionSeriesDecomposable
 import math.functions.logarithmic.BaseLogSeriesDecomposable
 import math.functions.logarithmic.NaturalLogSeriesDecomposable
+import math.functions.trigonometric.*
 import math.functions.trigonometric.CosSeriesDecomposable
+import math.functions.trigonometric.CscSeriesDecomposable
 import math.functions.trigonometric.SinSeriesDecomposable
 import math.functions.trigonometric.TanSeriesDecomposable
 import math.numbers.BigDecimalInfinityExtended
@@ -14,6 +16,8 @@ import java.util.function.Predicate
 
 fun main() {
     val csvWriter = CSVWriter("../../resources")
+   // val csvWriter = CSVWriter("/Users/zhenyamint/IdeaProjects/TPO 2/software-testing/Lab_2/src/main/resources")
+
 
     val naturalLogResults = mutableListOf<Pair<Double, Double>>()
     val log2Results = mutableListOf<Pair<Double, Double>>()
@@ -23,6 +27,7 @@ fun main() {
     val cosResults = mutableListOf<Pair<Double, Double>>()
     val cscResults = mutableListOf<Pair<Double, Double>>()
     val tanResults = mutableListOf<Pair<Double, Double>>()
+    val secResults = mutableListOf<Pair<Double, Double>>()
 
     val step = 0.1
     val start = 0.1
@@ -31,33 +36,36 @@ fun main() {
     val naturalLogSeries = NaturalLogSeriesDecomposable(0.001)
     val sinSeries = SinSeriesDecomposable(0.001)
     val cosSeries = CosSeriesDecomposable(0.001, sinSeries)
-    val cscSeries = CosSeriesDecomposable(0.001, sinSeries)
-    val tanSeries = TanSeriesDecomposable(sinSeries, cosSeries)
+    val cscSeries = CscSeriesDecomposable( sinSeries,0.001)
+    val secSeries = SecSeriesDecomposable(cosSeries,0.001)
+    val tanSeries = TanSeriesDecomposable(sinSeries, cosSeries, 0.001)
     val log2Series = BaseLogSeriesDecomposable(BigDecimalInfinityExtended(BigDecimal(2)), naturalLogSeries)
     val log3Series = BaseLogSeriesDecomposable(BigDecimalInfinityExtended(BigDecimal(3)), naturalLogSeries)
     val log5Series = BaseLogSeriesDecomposable(BigDecimalInfinityExtended(BigDecimal(5)), naturalLogSeries)
 
     var x = start
     while (x <= end) {
-        naturalLogResults.add(x to naturalLogSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
-        log2Results.add(x to log2Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
-        log3Results.add(x to log3Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
-        log5Results.add(x to log5Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
+       // naturalLogResults.add(x to naturalLogSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
+      //  log2Results.add(x to log2Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
+      //  log3Results.add(x to log3Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
+      //  log5Results.add(x to log5Series.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
         sinResults.add(x to sinSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
         cosResults.add(x to cosSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
         cscResults.add(x to cscSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
+        secResults.add(x to secSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
         tanResults.add(x to tanSeries.decompose(BigDecimalInfinityExtended(x), 0.001).toDouble())
         x += step
     }
 
-    csvWriter.write(naturalLogResults, "natural_log_results.csv")
-    csvWriter.write(log2Results, "log2_results.csv")
-    csvWriter.write(log3Results, "log3_results.csv")
-    csvWriter.write(log5Results, "log5_results.csv")
+   // csvWriter.write(naturalLogResults, "natural_log_results.csv")
+   // csvWriter.write(log2Results, "log2_results.csv")
+   // csvWriter.write(log3Results, "log3_results.csv")
+   // csvWriter.write(log5Results, "log5_results.csv")
     csvWriter.write(sinResults, "sin_results.csv")
     csvWriter.write(cosResults, "cos_results.csv")
     csvWriter.write(tanResults, "tan_results.csv")
     csvWriter.write(cscResults, "csc_results.csv")
+    csvWriter.write(secResults, "sec_results.csv")
 
     val p = PiecewiseFunctionSeriesDecomposable(
         listOf(
