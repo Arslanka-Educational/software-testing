e1210180953882d6b2c05d8546379b79fc3d646d
Zhenya Mint <zhenyamint@Zhenyas-Laptop.local>
2024-04-30 10:34:46 +0300

    add readCSV

--- Lab_2/src/main/kotlin/file/csv/CSVWriter.kt
+++ Lab_2/src/main/kotlin/file/csv/CSVWriter.kt
@@ -1,7 +1,10 @@
 package file.csv
 
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
 import java.io.IOException
+import java.math.BigDecimal
 
 class CSVWriter(private val folderPath: String) {
     fun write(data: List<Pair<Double, Double>>, filename: String) {
@@ -18,6 +21,23 @@ class CSVWriter(private val folderPath: String) {
             println("CSV файл успешно записан: ${file.absolutePath}")
         } catch (e: IOException) {
             println("Ошибка при записи в CSV файл $filename: ${e.message}")
+        }
+    }
+    companion object {
+        @JvmStatic
+        fun readCSV(fileName: String): List<List<Double>> {
+            val data = mutableListOf<List<Double>>()
+            val file = File(fileName)
+            file.bufferedReader().use { reader ->
+                var line = reader.readLine() // Skip header
+                line = reader.readLine() // Read first line of data
+                while (line != null) {
+                    val values = line.split(",").map { it.trim().toDouble() }
+                    data.add(values)
+                    line = reader.readLine()
+                }
+            }
+            return data
         }
     }
 }
